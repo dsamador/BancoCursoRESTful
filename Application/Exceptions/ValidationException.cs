98bddda86314dbd8d59ce77afa778583b9e2c139
David Amador <54488581+dsamador@users.noreply.github.com>
2022-03-04 12:57:17 -0500

    Exceptions

---
+++ Application/Exceptions/ValidationException.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        //mensaje generico que siempre se manda por la web api
+        public ValidationException() : base("Se han producido uno o mas errores de validacion")
+        {
+            Errors = new List<string>();
+
+        }
+        public List<string> Errors { get; }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            foreach (var failure in failures)
+            {
+                Errors.Add(failure.ErrorMessage);
+            }
+        }
+    }
+}
