8075f91c2ea520fead72ad87c39846a61288c636
David Amador <54488581+dsamador@users.noreply.github.com>
2022-03-04 10:58:38 -0500

    Configuring Application Layer

---
+++ Application/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            //Registra automaticamente los mapeos que haga en esta biblioteca de clase
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            
+            //FluentValidation
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            //MediatR
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
